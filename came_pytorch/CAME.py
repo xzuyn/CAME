c25e87a03fa4c632c2a3456262042c9695fbf81b
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-11-12 17:25:16 -0500

    Update CAME.py

--- came_pytorch/CAME.py
+++ came_pytorch/CAME.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from torch.optim import Optimizer
 
+HAS_TRITON = False
 try:
     import triton
     import triton.language as tl
@@ -92,9 +93,6 @@ try:
         dequantized_data = (quantized_data.to(tl.float32) / 255.0) * scale + min_val
 
         tl.store(output_ptr + offsets, dequantized_data, mask=mask)
-
-except ImportError:
-    HAS_TRITON = False
 
 
 class CAME(Optimizer):
