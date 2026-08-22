53d83c8ecf00a40aac38383241a060509d2fd29b
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-08-21 23:48:53 -0400

    add more overrides

--- came_pytorch/CAME.py
+++ came_pytorch/CAME.py
@@ -77,11 +77,20 @@ class CAME(torch.optim.Optimizer):
         return False
 
     def load_state_dict(self, state_dict):
+        allowed_overrides = [  # no betas
+            "lr",
+            "eps",
+            "clip_threshold",
+            "weight_decay",
+            "cautious_weight_decay",
+            "cautious_update",
+            "quantize_state",
+            "quant_block_size",
+            "quant_nbits",
+        ]
+        current_overrides = [{k: g[k] for k in allowed_overrides} for g in self.param_groups]
         super().load_state_dict(state_dict)
-        for group, overrides in zip(
-            self.param_groups,
-            [{k: g[k] for k in ["quantize_state", "quant_block_size", "quant_nbits"]} for g in self.param_groups],
-        ):
+        for group, overrides in zip(self.param_groups, current_overrides):
             group.update(overrides)
 
     def _get_options(self, param_shape):
